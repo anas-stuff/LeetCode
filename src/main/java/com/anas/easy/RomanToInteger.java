0ac2e0a6250fd61d7e28b82da13c284d80df71d5
Anas Elgarhy <anas.elgarhy.dev@gmail.com>
2022-05-06 22:03:10 +0200

    Addd Roman to Integer 🆑

--- src/main/java/com/anas/easy/RomanToInteger.java
+++ src/main/java/com/anas/easy/RomanToInteger.java
@@ -1,31 +1,41 @@
 package com.anas.easy;
-
 public class RomanToInteger {
     public int romanToInt(String s) {
         int num = 0;
         s = s.toLowerCase();
         for (int i = 0; i < s.length(); i++) {
-            switch (s.charAt(i)) {
-                case 'i' -> num++;
-                case 'v' -> num += v(s, i);
-                case 'x' -> num += x(s, i);
-                case 'l' -> num += 50;
-                case 'c' -> num += 100;
-                case 'd' -> num += 500;
-                case 'm' -> num += 1000;
-            }
+            num += getNum(s, i);
         }
         return num;
     }
 
-    private int x(String s, int i) {
-        if (i != 0 && s.charAt(i) ==)
+    private int getNum(String s, int i) {
+        int returnVal = getIntNum(s.charAt(i));
+        if (i != 0) {
+            short currenNum = (short) returnVal;
+            short prevNum = getIntNum(s.charAt(i - 1));
+            if (currenNum > prevNum) {
+                returnVal = 0;
+                returnVal -= prevNum;
+                returnVal += (currenNum - prevNum);
+            }
+        }
+        return returnVal;
     }
 
-    private int v(String s, int i) {
-        if (i != 0 && s.charAt(i - 1) == 'i')
-            return  4;
-        else
-            return 5;
+    private short getIntNum(char charAt) {
+        return switch (charAt) {
+            case 'v' -> 5;
+            case 'x' -> 10;
+            case 'l' -> 50;
+            case 'c' -> 100;
+            case 'd' -> 500;
+            case 'm' -> 1000;
+            default -> 1;
+        };
+    }
+
+    public static void main(String[] args) {
+        System.out.println("MCMXCIV = " + new RomanToInteger().romanToInt("MCMXCIV") );
     }
 }
